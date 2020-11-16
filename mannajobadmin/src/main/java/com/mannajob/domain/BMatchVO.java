5ff92719e713b64b13c481f7841396053197c36b
elon <aha15951@gmail.com>
2020-11-17 03:06:09 +0900

    error solved

--- mannajobadmin/src/main/java/com/mannajob/domain/BMatchVO.java
+++ mannajobadmin/src/main/java/com/mannajob/domain/BMatchVO.java
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-package com.mannajob.domain;
-
-import java.util.Date;
-
-import lombok.Data;
-
-@Data
-public class BMatchVO {
-	
-	private int b_num;
-	private String b_category;
-	private String m_id;
-	private String b_corp;
-	private String b_task;
-	private int b_price;
-	private String b_location;
-	private String b_stdate;
-	private String b_endate;
-	private String b_period;
-	private String b_del;
-	private String b_state;
-	private String b_subject;
-	private Date b_wdate;
-	private String b_contents;	
-}
-=======
 package com.mannajob.domain;
 
 import java.util.Date;
@@ -49,4 +22,3 @@ public class BMatchVO {
 	private Date b_wdate;
 	private String b_contents;
 }
->>>>>>> refs/remotes/origin/develope
