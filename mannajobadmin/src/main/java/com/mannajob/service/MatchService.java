1a7f5dd8a565e99067ec77974bbbe70db646ed58
elon <aha15951@gmail.com>
2020-11-20 03:13:11 +0900

    error

---
+++ mannajobadmin/src/main/java/com/mannajob/service/MatchService.java
@@ -0,0 +1,17 @@
+package com.mannajob.service;
+
+import java.util.List;
+
+import com.mannajob.domain.BMatchVO;
+import com.mannajob.domain.MatchVO;
+
+public interface MatchService {
+	
+	public List<MatchVO> searchBmatMat(int b_num);
+	
+	public List<BMatchVO> searchMat(String m_id);
+	
+	public List<BMatchVO> searchBMat(String m_id);
+	
+
+}
